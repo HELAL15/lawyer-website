31210adcdc2fb548ee72abc72e8fe644d4356852
Ahmed Helal <87047003+HELAL15@users.noreply.github.com>
2023-07-17 21:11:56 +0300

    lawyer website

--- js/main.js
+++ js/main.js
@@ -1,24 +1,75 @@
 
-let play = document.querySelector('#play');
-let pause = document.querySelector('#pause');
-let video = document.querySelector('#video');
-let imgPause = document.querySelector('.for-pause');
 
-play.addEventListener('click', () => {
-  play.classList.add('d-none');
-  pause.classList.remove('d-none');
+
+
+
+
+
+const play_v = document.querySelector('#play');
+const pause_v = document.querySelector('#pause');
+
+function play(){
+  var video = document.querySelector('#video');
+var imgPause = document.querySelector('.for-pause');
+  play_v.classList.add('d-none');
+  pause_v.classList.remove('d-none');
   imgPause.classList.add('hide');
   video.play();
-});
+  console.log('clicked')
+}
 
-pause.addEventListener('click', () => {
-  play.classList.remove('d-none');
-  pause.classList.add('d-none');
+
+function pause(){
+  var video = document.querySelector('#video');
+var imgPause = document.querySelector('.for-pause');
+  play_v.classList.remove('d-none');
+  pause_v.classList.add('d-none');
   imgPause.classList.remove('hide');
   video.pause();
-});
+}
+
+// play_v.addEventListener('click', () => {
+//   var video = document.querySelector('#video');
+// var imgPause = document.querySelector('.for-pause');
+//   play_v.classList.add('d-none');
+//   pause_v.classList.remove('d-none');
+//   imgPause.classList.add('hide');
+//   video.play();
+//   console.log('clicked')
+// });
+
+// pause_v.addEventListener('click', () => {
+//   var video = document.querySelector('#video');
+// var imgPause = document.querySelector('.for-pause');
+//   play_v.classList.remove('d-none');
+//   pause_v.classList.add('d-none');
+//   imgPause.classList.remove('hide');
+//   video.pause();
+// });
 
 
+
+
+const showContent = document.getElementById('show-content');
+console.log(showContent);
+
+// function showContent(){
+//   let hideContent = document.querySelector('.content');
+//   let down_up = document.getElementById('down-up');
+//   console.log('clicked');
+//   hideContent.classList.toggle('d-none');
+//   down_up.classList.toggle('fa-caret-down');
+//   down_up.classList.toggle('fa-caret-up');
+// }
+
+showContent.addEventListener('click' , ()=>{
+  let hideContent = document.querySelector('.content');
+  let down_up = document.getElementById('down-up');
+  console.log('clicked');
+  hideContent.classList.toggle('d-none');
+  down_up.classList.toggle('fa-caret-down');
+  down_up.classList.toggle('fa-caret-up');
+})
 
 $('.owl-carousel.our-team').owlCarousel({
   rtl: true,
@@ -62,3 +113,4 @@ $('.owl-carousel').owlCarousel({
       }
   }
 })
+
